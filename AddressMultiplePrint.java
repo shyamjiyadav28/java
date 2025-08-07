6ca991fecaba213fd1a08102289cf341644ca333
shyamjiyadav28 <shyamji.yadav.cs28@iilm.edu>
2025-08-07 15:37:22 +0530

    Add files via upload

---
+++ AddressMultiplePrint.java
@@ -0,0 +1,9 @@
+public class AddressMultiplePrint {
+    public static void main(String[] args) {
+        System.out.println("Shyamji Yadav");
+        System.out.println("6B Street");
+        System.out.println("kanpur  City");
+        System.out.println("Uttar Pradesh - 210507");
+    }
+}
+
