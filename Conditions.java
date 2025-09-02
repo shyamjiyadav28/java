9826f8b35ebcee504c6b27cd99c6d29189430604
shyamjiyadav28 <shyamji.yadav.cs28@iilm.edu>
2025-09-02 08:41:35 +0530

    Add files via upload

---
+++ Conditions.java
@@ -0,0 +1,12 @@
+public class Conditions {
+    public static void main(String[] args) {
+    int a =18;
+    if(a != 18){
+        System.out.println("you can drive");
+    }
+    else{
+        System.out.println("you cannot drive");
+    }
+    }    
+    
+}
