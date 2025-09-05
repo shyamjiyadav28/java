4ffd042b1e40637512f98e481f9e24f90b7cfc57
shyamjiyadav28 <shyamji.yadav.cs28@iilm.edu>
2025-09-05 22:37:39 +0530

    Add files via upload

---
+++ DoWhile.java
@@ -0,0 +1,15 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int a =10;
+        //while(a<5){
+          //  System.out.println(a);
+          //  a++;
+        
+        do { 
+            System.out.println(a);
+            a++;
+        } while(a<5);
+            
+
+}
+}
