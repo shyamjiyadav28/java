0d8e6a12bddead0dc9473c052164c75624829a17
shyamjiyadav28 <shyamji.yadav.cs28@iilm.edu>
2025-09-05 22:37:19 +0530

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,12 @@
+public class Forloop {
+    public static void main(String[] args) {
+        //int n = 5;
+        //for(int i=0;i<=5;i++){
+            //System.out.println(2*i+1);
+            for(int i =10;i>=0;i--){
+                System.out.println(i);
+
+        }
+    }
+    
+}
