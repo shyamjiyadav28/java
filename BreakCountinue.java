e6ef2c9a32769a62837f38b243e2c7547ce543ab
shyamjiyadav28 <shyamji.yadav.cs28@iilm.edu>
2025-09-05 22:38:38 +0530

    Add files via upload

---
+++ BreakCountinue.java
@@ -0,0 +1,18 @@
+public class BreakCountinue {
+    public static void main(String[] args) {
+        //for(int i=1;i<=10;i++){
+           // if(i==5){
+             //   break;
+            //}
+//System.out.println(i);
+for(int i=1;i<=10;i++){
+            if(i==5){
+                continue;   
+            }
+            System.out.println(i);
+        }
+        }
+    }
+
+    
+
