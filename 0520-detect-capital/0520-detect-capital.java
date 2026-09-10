7976a99d6d30998ac65589092444f92ddf9dd0f8
Shyamji Yadav <shyamji.yadav.cs28@iilm.edu>
2026-09-10 10:13:02 +0530

    Time: 1 ms (88.37%), Space: 43.1 MB (77.62%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word.equals(word.toUpperCase())){
+            return true;
+        }
+        if (word.equals(word.toLowerCase())){
+        return true;
+    }
+    String first = word.substring(0,1);
+    String rest  = word.substring(1);
+    if(first.equals(first.toUpperCase())&& rest.equals(rest.toLowerCase())){
+        return true;
+    }  
+    return false;
+    }
+
+}
